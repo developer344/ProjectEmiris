6e00677d186d2dd6e7f5ddc529ab2241708ba9eb
giannisgeo344 <giannisgeo344@gmail.com>
2021-10-19 20:44:17 +0300

    hello

--- lsh.cpp
+++ lsh.cpp
@@ -5,4 +5,8 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
+    //./lsh–i<inputfile> –q<queryfile> –k<int> -L<int> -ο<outputfile> -Ν<numberofnearest> -R<radius>
+    if (!(argc % 2) || argc > 15 || argc < 7)
+    {
+    }
 }
